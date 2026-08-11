b5f5e4f4e9c058564cfc9b8f1df234c9ea8f2bc2
LilGazi <godslovemarcelo@gmail.com>
2026-08-12 00:36:46 +0100

    Implement number guessing game

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -1,1 +1,15 @@
+import random
 
+secret_number = random.randint(1, 100)
+
+guess = int(input("Guess a number from 1 to 100: "))
+
+while guess != secret_number:
+    if guess < secret_number:
+        print("Too low!")
+    else:
+        print("Too high!")
+
+    guess = int(input("Try again: "))
+
+print("Congratulations! You guessed the number!")
